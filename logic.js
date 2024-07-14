3369e16a575d125c8eaba78f7e62ce6e65083ba8
Pankaj Kumar <135860999+pankajjaat2004@users.noreply.github.com>
2024-07-14 16:22:09 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,20 +1,12 @@
 const keys = document.querySelectorAll('.key');
 
-// keys.forEach(key => {
-//     key.addEventListener('click', () => playSound(key));
-// });
-
 for(let i=0 ;i<keys.length;i++){
     keys[i].addEventListener('click',playSound);
 }
 
 function playSound() {
     const note = this.dataset.note;
-    console.log('sound played');
-    console.log(note);
     const audio = new Audio(`audio/${note}.mp3`);
-    // const audio=new Audio('audio/A1.mp3');
     audio.volume=1.0;
-    // audio.playbackRate=2;
     audio.play();
 }
